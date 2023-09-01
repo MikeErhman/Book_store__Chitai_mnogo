bad11aa3988d8f5e78ca2c9deb5ae25933135073
MikeErhman <mihailzhironkin@yandex.ru>
2023-09-01 09:10:38 +0300

    Final view 1

---
+++ script/script.js
@@ -0,0 +1,13 @@
+// const swiper = new Swiper('.swiper', {
+//   // Optional parameters
+//   direction: 'vertical',
+//   loop: true,
+
+  const swiper = new Swiper(".swiper", {
+    // effect: "fade",
+ 
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false,
+    },
+  });
