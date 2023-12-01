0211641c418d6f5d58cb916ef17cd7b526858c28
MikeErhman <mihailzhironkin@yandex.ru>
2023-12-01 21:41:09 +0900

    script sandwich done

--- script/sandwich_script.js
+++ script/sandwich_script.js
@@ -1,7 +1,7 @@
 var sandwich = document.getElementById("sandwich")
+var sandwich_is_active = document.getElementById("sandwich_is_active")
 
-function sandwich_ckick(clk){
+
     sandwich.addEventListener("click", function(){
-        
+        sandwich_is_active.classList.toggle("sandwich_wrapper")
     })
-}
