c939675660301b33861a82bca83a277cb8f69d3c
MikeErhman <mihailzhironkin@yandex.ru>
2023-12-18 16:26:39 +0900

    push

--- script/page_book-script.js
+++ script/page_book-script.js
@@ -7,22 +7,22 @@
 
 
 
-var about_book_desk = document.getElementById("about_book-id-desk")
+var about_book_desk = document.getElementById("about_book-id-desk");
 var about_book = document.getElementById("about_book-id");
-var about_book_div_desk = document.getElementById("about_book_div-desk")
-var about_book_div = document.getElementById("about_book_div")
-var quotes_desk = document.getElementById("quotes-id-desk")
+var about_book_div_desk = document.getElementById("about_book_div-desk");
+var about_book_div = document.getElementById("about_book_div");
+var quotes_desk = document.getElementById("quotes-id-desk");
 var quotes = document.getElementById("quotes-id");
-var quotes_book_div_desk = document.getElementById("quotes_div-desk")
-var quotes_div = document.getElementById("quotes_div")
-var notes_desk = document.getElementById("notes-id-desk")
+var quotes_book_div_desk = document.getElementById("quotes_div-desk");
+var quotes_div = document.getElementById("quotes_div");
+var notes_desk = document.getElementById("notes-id-desk");
 var notes = document.getElementById("notes-id");
-var notes_div_desk = document.getElementById("notes_div-desk")
-var notes_div = document.getElementById("notes_div")
+var notes_div_desk = document.getElementById("notes_div-desk");
+var notes_div = document.getElementById("notes_div");
 var book_description = document.getElementById("title_descript-id");
 var book_description_text = document.getElementById("text_descript-id");
-var naw_square = document.getElementById("naw_sqre-id")
-var naw_square_desk = document.getElementById("naw_sqre-id_desk")
+var naw_square = document.getElementById("naw_sqre-id");
+var naw_square_desk = document.getElementById("naw_sqre-id_desk");
 
 // about_book.classList.add("display")
 // book_description.classList.add("display")
@@ -52,20 +52,20 @@ buttons_desk.forEach(function(button){
 	button.addEventListener('click', function(){
 		console.log(button +" _desk is click")
 		switch(true){
-			case button.id == "about_book-id-desk" : if(naw_square.className != "naw_sqre-left"){
+			case button.id == "about_book-id-desk" : if(naw_square_desk.className != "naw_sqre-left"){
 				console.log("if accepted - left")
 				toggle_divs_desk(button); 
-				naw_square.className = "naw_sqre-left"
+				naw_square_desk.className = "naw_sqre-left"
 			};break
-			case button.id == "quotes_div-desk" : if(naw_square.className != "naw_sqre-center"){
+			case button.id == "quotes-id-desk" : if(naw_square_desk.className != "naw_sqre-center"){
 				console.log("if accepted - center")
 				toggle_divs_desk(button); 
-				naw_square.className = "naw_sqre-center"
+				naw_square_desk.className = "naw_sqre-center"
 			};break
-			case button.id == "notes-id-desk" : if(naw_square.className != "naw_sqre-right"){
+			case button.id == "notes-id-desk" : if(naw_square_desk.className != "naw_sqre-right"){
 				console.log("if accepted - right")
 				toggle_divs_desk(button);
-				naw_square.className = "naw_sqre-right"
+				naw_square_desk.className = "naw_sqre-right"
 			};break
 		}
 	})
