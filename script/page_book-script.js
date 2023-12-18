e2d204bfb3e4de2592e8411d781699413552307b
MikeErhman <mihailzhironkin@yandex.ru>
2023-12-18 12:25:13 +0900

    try classname

--- script/page_book-script.js
+++ script/page_book-script.js
@@ -7,16 +7,22 @@
 
 
 
-
+var about_book_desk = document.getElementById("about_book-id-desk")
 var about_book = document.getElementById("about_book-id");
-var about_book_div =document.getElementById("about_book_div")
+var about_book_div_desk = document.getElementById("quotes_div-desk")
+var about_book_div = document.getElementById("about_book_div")
+var quotes_desk = document.getElementById("quotes-id-desk")
 var quotes = document.getElementById("quotes-id");
+var quotes_book_div_desk = document.getElementById("quotes_div-desk")
 var quotes_div = document.getElementById("quotes_div")
+var notes_desk = document.getElementById("notes-id-desk")
 var notes = document.getElementById("notes-id");
+var notes_div_desk = document.getElementById("notes_div-desk")
 var notes_div = document.getElementById("notes_div")
 var book_description = document.getElementById("title_descript-id");
 var book_description_text = document.getElementById("text_descript-id");
 var naw_square = document.getElementById("naw_sqre-id")
+var naw_square_desk = document.getElementById("naw_sqre-id_desk")
 
 // about_book.classList.add("display")
 // book_description.classList.add("display")
@@ -28,7 +34,7 @@ console.log(book_description.classList)
 console.log("SQRE " + naw_square.classList)
 
 
-var buttons = [about_book, quotes, notes]
+var buttons = [about_book, quotes, notes, about_book_desk, quotes_desk, notes_desk]
 
 function define_button(button){
 	naw_square.classList.remove('"'+butt+'"')
@@ -45,40 +51,55 @@ buttons.forEach(function(button){
 	button.addEventListener('click', function(){
 		console.log(button + " is click")
 		switch(true){
-			case button.id == "about_book-id" : if(naw_square.className != "naw_sqre-left") {
+			case button.id == "about_book-id" || "about_book-id-desk" : if(naw_square.className != "naw_sqre-left" && naw_square_desk.className != "naw_sqre-left") {
 				toggle_divs(button); 
-				naw_square.className = "naw_sqre-left"};
+				naw_square.className = "naw_sqre-left"
+				naw_square_desk.className = "naw_sqre-left"
+			};
 				break
-			case button.id == "quotes-id" : if(naw_square.className != "naw_sqre-center"){
+			case button.id == "quotes-id" || "quotes-id-desk" : if(naw_square.className != "naw_sqre-center" && naw_square_desk.className != "naw_sqre-center"){
 				toggle_divs(button); 
-				naw_square.className = "naw_sqre-center"};
+				naw_square.className = "naw_sqre-center"
+				naw_square_desk.className = "naw_sqre-center"
+			};
 				break
-			case button.id == "notes-id" : if(naw_square.className != "naw_sqre-right"){
+			case button.id == "notes-id" || "notes-id-desk" : if(naw_square.className != "naw_sqre-right" && naw_square_desk.className != "naw_sqre-right"){
 				toggle_divs(button);
-				naw_square.className = "naw_sqre-right"};
+				naw_square.className = "naw_sqre-right"
+				naw_square_desk.className = "naw_sqre-right"
+			};
 				break
 			
 		}
 	})
 	function toggle_divs(div){
 		switch(true){
-			case div.id == "about_book-id" : if (naw_square.className != "naw_sqre-left"){ 
+			case div.id == "about_book-id" || "about_book-id-desk" : if (naw_square.className != "naw_sqre-left" && naw_square_desk.className != "naw_sqre-left"){ 
 				console.log("left");
 				notes_div.classList = "inactive";
+				notes_div_desk.classList = "inactive";
 				quotes_div.classList = "inactive";
+				quotes_book_div_desk.classList = "inactive";
 				about_book_div.classList = "about_book-active"
+				about_book_div_desk.classList = "about_book-active"
 		};break
-			case div.id == "quotes-id" : if(naw_square.className != "naw_sqre-center"){
+			case div.id == "quotes-id" || "quotes-id-desk" : if(naw_square.className != "naw_sqre-center" && naw_square_desk.className != "naw_sqre-center"){
 				console.log("center");
 				about_book_div.classList = "inactive"; 
+				about_book_div_desk.classList = "inactive"; 
 				notes_div.classList = "inactive";
-				quotes_div.classList = "quotes_book_div-active" 
+				notes_div_desk.classList = "inactive";
+				quotes_div.classList = "quotes_book_div-active"
+				quotes_book_div_desk.classList = "quotes_book_div-active" 
 		}; break
-			case div.id == "notes-id" : if(naw_square.className != "naw_sqre-right"){
+			case div.id == "notes-id" || "notes-id-desk" : if(naw_square.className != "naw_sqre-right" && naw_square_desk.className != "naw_sqre-right"){
 				console.log("right");
-				about_book_div.classList = "inactive"; 
+				about_book_div.classList = "inactive";
+				about_book_div_desk.classList = "inactive";
 				quotes_div.classList = "inactive";
+				quotes_book_div_desk.classList = "inactive";
 				notes_div.classList = "notes_div"
+				notes_div_desk.classList = "notes_div"
 		}; break
 
 		}
