175a03600a88bd6e941ca44cb77914d6f8fe7d08
MikeErhman <mihailzhironkin@yandex.ru>
2024-01-03 16:55:32 +0900

    search done

--- script/live_search.js
+++ script/live_search.js
@@ -3,30 +3,21 @@ document.querySelector('#elastic').oninput = function(){
     let eclastic_items = document.querySelectorAll(".elastic li");
     if(val != ''){
         eclastic_items.forEach(function(elem){
-            if(elem.innerText.search(val) == -1){
-                elem.classList.add("hide");
-                elem.innerHTML = elem.innerText
+            if(elem.innerText.search(val) != -1 && elem.innerText != ''){
+                elem.classList.remove("hide");
+    
             }
             else {
-                elem.classList.remove("hide");
-                let str = elem.innerText
-                elem.innerHTML = insert_mark(str, elem.innerText.search(val), val.length);
+                elem.classList.add("hide");
+
             }
         });
     }
     else{
         eclastic_items.forEach(function (elem){ 
-            elem.classList.remove("hide")
-            elem.innerHTML = elem.innerText
+            elem.classList.add("hide")
+            
         })
         }
            
-    }
-
-
-    function insert_mark(string, pos, len){
-        // hello world
-        // hello<mark> wo </mark>rld
-        //hello + <mark> + wo + </mark> + rld
-        return string.slice(0, pos) + "<mark>" + string.slice(pos, pos + len) + "</mark>" + string.slice(pos+len);
     }
