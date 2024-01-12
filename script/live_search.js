f12629f2d861c510e2de004cb010cc918ab78f26
MikeErhman <mihailzhironkin@yandex.ru>
2024-01-12 13:44:40 +0900

    Search done!!

--- script/live_search.js
+++ script/live_search.js
@@ -1,41 +1,14 @@
+let valueOfId 
 
-document.querySelector('#elastic_mob').oninput = function(){
-    // let val =  this.value.trim();
-    // let eclastic_items = document.querySelectorAll(".elastic li");
-    // let elastic_items_mob = document.querySelectorAll(".elastic_mob li")
-    // if(val != ''){
-    //     elastic_items_mob.forEach(function(elem){
-    //         if(elem.innerText.search(val) != -1 && elem.innerText != ''){
-    //             elem.classList.remove("hide");
-    
-    //         }
-    //         else {
-    //             elem.classList.add("hide");
-
-    //         }
-    //     });
-    //     eclastic_items.forEach(function(elem){
-    //         if(elem.innerText.search(val) != -1 && elem.innerText != ''){
-    //             elem.classList.remove("hide");
-    
-    //         }
-    //         else {
-    //             elem.classList.add("hide");
-
-    //         }
-    //     });
-    // }
-    // else{
-    //     eclastic_items.forEach(function (elem){ 
-    //         elem.classList.add("hide")
-            
-    //     })
-    //     }
-           
-    
-        let val =  this.value.trim();
-        let eclastic_items = document.querySelectorAll(".elastic li");
-        let elastic_items_mob = document.querySelectorAll(".eclastic_items_mob li")
+console.log(window.innerWidth);
+window.addEventListener("click", function(cl){
+    valueOfId = "#" + cl.target.id 
+function elastic(inputId){
+document.querySelector(inputId).addEventListener("input", function(){
+    console.log("input")
+        let val =  this.value.trim(),
+        elastic_items = document.querySelectorAll(".elastic li"),
+        elastic_items_mob = document.querySelectorAll(".elastic_items_mob li");
     function elasticSearch(elasticContent){
         
         if(val != ''){
@@ -58,10 +31,19 @@ document.querySelector('#elastic_mob').oninput = function(){
         }
            
 }
-elasticSearch(elastic_items_mob);
-elasticSearch(eclastic_items);
 
+if(window.innerWidth > 500){
+    elasticSearch(elastic_items);
 }
+else{
+    elasticSearch(elastic_items_mob);
+}
+
+
+})
+}
+elastic(String(valueOfId))
+})
 
 
 var inp_text = document.querySelector('#text')
@@ -75,9 +57,8 @@ var body = document.querySelector("body")
 
 
 body.addEventListener("click", function(click){
-    console.log(click)
     if(click.target.id == "text" ){
-        console.log("input click");
+        console.log("input click"); 
         logo_search_buttons.style.display = ("none")
         text_wide_mob.style.display = ("block")
         inp_butt_wide.style.display = ("block")
